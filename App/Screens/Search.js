a96bb439436b532bd390a9514016adc235dce789
Adrianna Valdivia <avaldivi@ggc.edu>
2017-07-13 11:47:15 -0400

    Change handleSubmit function to navigate to Dashboard with correct attributes (John)

--- App/Screens/Search.js
+++ App/Screens/Search.js
@@ -64,41 +64,54 @@ class Search extends Component {
       isLoading: false,
       error: false
     }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
+
+  // componentWillMount() {
+  //   isSignedIn()
+  //     .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+  //     .catch(err => alert("An error occurred"));
+  // }
+
   handleChange(event) {
     this.setState({
       username: event.nativeEvent.text
     });
   }
+
   handleSubmit(){
     //UPDATE our IndicatorIOS spinner
     this.setState({
       isLoading: true
     });
+
+    let scope = this
     //FETCH data from github
     api.getBio(this.state.username)
       .then((res) => {
         if (res.message === 'Not found') {
-          this.setState({
+          scope.setState({
             error: 'User not found',
             isLoading: false
           })
         } else {
-          //REROUTE to the next passing github information
-          this.props.navigator.push({
+          console.log(res)
+          scope.props.navigation.navigate( 'Dashboard', {
             title: res.name || 'Select an Option',
-            component: Dashboard,
-            passProps: {userInfo: res}
+            userInfo: res
           });
-          this.setState({
+          scope.setState({
             isLoading: false,
             error: false,
             username: ''
           })
         }
-  });
-}
+    });
+  }
+
   render() {
+    //const { navigation } = this.props; 
     return (
       <View style={styles.mainContainer}>
         <Text style={styles.title}> Search for a GitHub User </Text>
@@ -108,7 +121,7 @@ class Search extends Component {
           onChange={this.handleChange.bind(this)} />
         <TouchableHighlight
           style={styles.button}
-          onPress={this.handleSubmit.bind(this)}
+          onPress={() => this.handleSubmit()}
           underlayColor="white">
           <Text style={styles.buttonText}> Search </Text>
         </TouchableHighlight>
